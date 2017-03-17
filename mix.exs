c88e224ce184f3ca47b1736d66a0e49f9f7a7157
Sven Gehring <cbrxde@gmail.com>
2017-03-17 09:09:01 +0100

    Add some metadata

--- mix.exs
+++ mix.exs
@@ -2,12 +2,29 @@ defmodule InwxDomrobot.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :inwx_domrobot,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :inwx_domrobot,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+
+      name: "INWX DomRobot",
+      description: "Simple interface for INWX DomRobot in Elixir",
+      source_url: "https://github.com/cybrox/inwx-domrobot",
+      package: package()
+   ]
+  end
+
+
+  # Package
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Sven Gehring <cbrxde@gmail.com>"],
+      links: %{github: "https://github.com/cybrox/inwx-domrobot"}
+    ]
   end
 
 
